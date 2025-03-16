03fd0456a8163cfa43aba55b9380e80703faf6cb
Kowsalya-SE <155236764+Kowsalya-SE@users.noreply.github.com>
2025-03-16 07:14:36 +0000

    file list

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -1,7 +1,18 @@
 const httpstatus = require('../utils/httpstatus.js');
 const filestore = require('../utils/filestore.js');
+const { knex } = require('../config/database.js');
+const dayjs = require('dayjs');
 
 class uploadFunctions{
+    async getFiles(req,res){
+        try{
+            let getLogs = await getFilelist();
+            return httpstatus.successResponse({message:"Files fetched successfully!..",files:getLogs},res)
+        } catch (e){
+            console.log(e,'e')
+            return httpstatus.errorResponse({message:"Internal server error"},res);
+        }
+    }
     async uploadFile(req,res){
         try {
             req.body.file = req.files;
@@ -16,8 +27,17 @@ class uploadFunctions{
             }          
 
             const file = await filestore.uploadGetDocumentPath(req, res);
-            const response = {
-                filepath : file.filepath[0].filepath
+            const insertDb = await knex('files_lists').insert({
+                file_name:file.filepath[0].filename,
+                file_path:file.filepath[0].filepath,
+                created_at:new Date(),
+                updated_at:new Date()
+            })
+            const getfiles = await getFilelist();
+            let response = {
+                id:insertDb[0],
+                filepath : file.filepath[0].filepath,
+                files:getfiles
             }
             return httpstatus.successResponse({message:file.status,response},res);
         } catch (e) {
@@ -26,5 +46,12 @@ class uploadFunctions{
         }
     }
 }
-
+async function getFilelist(params) {
+    let getLists = await knex('files_lists');
+    for(const data of getLists){
+        data.created_at = dayjs(data.created_at).format('ddd, MMM D, YYYY h:mm A');
+        data.file_path = `${process.env.API_URL}${data.file_path}`
+    }
+    return getLists;
+}
 module.exports = new  uploadFunctions();
